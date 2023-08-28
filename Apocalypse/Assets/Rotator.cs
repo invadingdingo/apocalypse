1351967123bec269e8878b476183d29d6ec48778
Sam <63943054+invadingdingo@users.noreply.github.com>
2023-08-28 16:05:40 -0500

    ??

--- Apocalypse/Assets/Rotator.cs
+++ Apocalypse/Assets/Rotator.cs
@@ -7,7 +7,7 @@ public class Rotator : MonoBehaviour
     public bool canRotate = false;
 
     void Rotate() {
-        if (canRotate && Input.GetKey("W")) {
+        if (canRotate && Input.GetKeyDown("W")) {
             transform.rotation = transform.rotation * Quaternion.Euler(0f, 90f, 0f);
         }
     }
